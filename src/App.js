b5cc439d586cf72a64df72ad76ad2c228be1f6bf
Minjun Youn <minjun1.y@partner.samsung.com>
2019-03-22 16:46:50 -0500

    Refactor Apps - remove manager related work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import styled, { createGlobalStyle } from 'styled-components'
 import Routes from './Routes'
-import ManagerRoutes from './ManagerRoutes'
 import { Auth, API } from 'aws-amplify'
 import Theme from './theme'
 import { ThemeProvider } from 'styled-components'
@@ -33,7 +32,6 @@ class App extends Component {
     this.state = {
       isAuthenticating: true,
       isAuthenticated: false,
-      uid: null,
       resident: null,
       theme: Theme.Basic
     }
@@ -50,38 +48,20 @@ class App extends Component {
     this.setState({ isAuthenticating: false })
   }
 
-
   userHasAuthenticated = async (uid) => {
     let resident = null
-    try {
-      resident = await API.get('apt', `/residents/${uid}`)
-    } catch (e) {
-      console.log(e)
-      console.log("Error response : ", e.response)
+    if (uid) {
+      try {
+        resident = await API.get('apt', `/residents/${uid}`)
+      } catch (e) {
+        console.log(e, e.response)
+      }
     }
 
     this.setState({
       isAuthenticated: uid ? true : false,
-      uid,
       resident
     })
-  }
-
-  managerHasAuthenticated = async (uid) => {
-    console.log(uid)
-    if (uid === "58e15cde-7dca-4453-9336-0bd368960c9e") {
-      this.setState({ isManagerAuthenticated: true })
-    } else {
-      this.setState({ isManagerAuthenticated: false })
-
-      try {
-        await Auth.signOut()
-      } catch (e) {
-        console.log(e)
-        alert(e.message)
-      }
-      alert("Please use manager account")
-    }
   }
 
   handleLogout = async () => {
@@ -94,20 +74,9 @@ class App extends Component {
     }
   }
 
-  handleManagerLogout = async () => {
-    try {
-      await Auth.signOut()
-      this.managerHasAuthenticated(false)
-    } catch (e) {
-      console.log(e)
-      alert(e.message)
-    }
-  }
-
-  renderResident() {
+  render() {
     const childProps = {
       isAuthenticated: this.state.isAuthenticated,
-      uid: this.state.uid,
       userHasAuthenticated: this.userHasAuthenticated,
       resident: this.state.resident,
       theme: this.state.theme
@@ -137,44 +106,6 @@ class App extends Component {
         </StyledContainer>
       </ThemeProvider>
     );
-  }
-
-  renderManager() {
-    const childProps = {
-      isManagerAuthenticated: this.state.isManagerAuthenticated,
-      managerHasAuthenticated: this.managerHasAuthenticated,
-      theme: this.state.theme
-    }
-    return (
-      !this.state.isAuthenticating &&
-      <ThemeProvider theme={this.state.theme}>
-        <StyledContainer>
-          <Navbar variant="light" bg="light" expand="md">
-            <Navbar.Brand>
-              <Link to='/manager'>SAVOY Management</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-              <Nav className="ml-auto">
-                {this.state.isManagerAuthenticated
-                  ? <Nav.Link onClick={this.handleManagerLogout}>Manager Logout</Nav.Link>
-                  : <LinkContainer to='/manager/login'>
-                    <Nav.Link>Manager Login</Nav.Link>
-                  </LinkContainer>
-                }
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <ManagerRoutes childProps={childProps} />
-        </StyledContainer>
-      </ThemeProvider>
-    );
-  }
-
-  render() {
-    return (/^\/manager/.test(this.props.location.pathname))
-      ? this.renderManager()
-      : this.renderResident()
   }
 }
 
